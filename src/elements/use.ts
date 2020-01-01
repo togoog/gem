50924d67ebdf9172fd95af2dde3b33e29d67911b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-01 08:53:32 +0000

    Deploy mantou132/gem to github.com/mantou132/gem.git:gh-pages

--- src/elements/use.ts
+++ src/elements/use.ts
@@ -1,11 +1,30 @@
-import { GemElement, html } from '../';
+import { GemElement, html, attribute, property, customElement } from '../';
 
+/**
+ * svg 中的 `<use>` 不能穿透 ShadowDOM,
+ * 此元素用来模拟 `<use>`,
+ * 由于是复制元素，所以不能像 `<use>` 一样自动更新
+ *
+ * @attr ref
+ */
+@customElement('gem-use')
 export class Use extends GemElement {
-  /**@attr */ ref: string; // CSS 选择器
-  static observedAttributes = ['ref'];
+  @attribute ref: string; // CSS 选择器
+  @property root: HTMLElement | Document | ShadowRoot = document;
+
+  private getContent() {
+    // 只支持 `svg` 或者 `template > svg`
+    const ele = this.root.querySelector(this.ref);
+    if (ele instanceof HTMLTemplateElement) {
+      return ele.content.cloneNode(true);
+    } else if (ele instanceof SVGSVGElement) {
+      return ele.cloneNode(true);
+    } else {
+      return null;
+    }
+  }
 
   render() {
-    const template = document.querySelector(this.ref);
     return html`
       <style>
         :host {
@@ -14,11 +33,13 @@ export class Use extends GemElement {
         :host(:not([hidden])) {
           display: inline-block;
         }
+        svg {
+          width: 100%;
+          height: 100%;
+        }
       </style>
-      ${template && template.cloneNode(true)}
+      ${this.getContent()}
       <slot></slot>
     `;
   }
 }
-
-customElements.define('gem-use', Use);
